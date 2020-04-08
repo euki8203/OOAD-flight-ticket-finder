bd94be4f86512c251a896905ae16e91285eeb314
Ashley Kim <ashleykim0506@gmail.com>
2020-04-08 12:17:12 -0600

    resolved undefined bug

--- api/request.js
+++ api/request.js
@@ -5,23 +5,17 @@ const fetch = require("node-fetch");
 
 
 app.get('/findFlight', function(req, res) {
-	console.log("in function")
 	const { inboundDate, originPlace, destinationPlace, outboundDate, country}  = req.query;
 	let currency = "USD";
 	let locale = "en-US";
 
-	console.log("inboundDate: ", inboundDate)
-	console.log("originPlace: ", originPlace)
-	console.log("destinationPlace: ", destinationPlace)
-	console.log("outboundDate: ", outboundDate)
-	console.log("country: ", country)
+	returnDate = (inboundDate === 'undefined') ?  new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toISOString().slice(0,10) : inboundDate.toString()
+	bodyCountry = (country === 'undefined') ? "US" : country.toString()
+	bodyOriginPlace = (originPlace === 'undefined') ? "DEN-sky" : originPlace.toString()
+	bodyDestinationPlace = (destinationPlace === 'undefined') ? "SFO-sky" : destinationPlace.toString()
+	departureDate = (outboundDate === 'undefined') ? new Date().toISOString().slice(0,10) : outboundDate.toString()
 
-
-	returnDate = (inboundDate == undefined) ?  new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toISOString().slice(0,10) : inboundDate.toString()
-	bodyCountry = (country == undefined) ? "US" : country.toString()
-	bodyOriginPlace = (originPlace == undefined) ? "DEN-sky" : originPlace.toString()
-	bodyDestinationPlace = (destinationPlace == undefined) ? "SFO-sky" : destinationPlace.toString()
-	departureDate = (outboundDate == undefined) ? new Date().toISOString().slice(0,10) : outboundDate.toString()
+	//console.log(returnDate, bodyCountry, bodyOriginPlace, bodyDestinationPlace, departureDate)
 
 	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/"+bodyCountry+"/"+currency+
 		"/"+locale+"/"+bodyOriginPlace+"/"+bodyDestinationPlace+"/"+departureDate+"?inboundpartialdate="+returnDate, {
