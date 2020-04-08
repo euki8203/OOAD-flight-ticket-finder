0e00a257fdd673a28e8cd2858272ab6e5cfe23c4
Ashley Kim <ashleykim0506@gmail.com>
2020-04-08 00:35:03 -0600

    adjusted endpoint

--- api/request.js
+++ api/request.js
@@ -4,20 +4,27 @@ var constants = require('./constants');
 const fetch = require("node-fetch");
 
 
-app.post('/findFlight', function(req, res) {
-	const { inboundDate, country, currency, locale, originPlace,
-			destinationPlace, outboundDate}  = req.query;
+app.get('/findFlight', function(req, res) {
+	console.log("in function")
+	const { inboundDate, originPlace, destinationPlace, outboundDate, country}  = req.query;
+	let currency = "USD";
+	let locale = "en-US";
+
+	console.log("inboundDate: ", inboundDate)
+	console.log("originPlace: ", originPlace)
+	console.log("destinationPlace: ", destinationPlace)
+	console.log("outboundDate: ", outboundDate)
+	console.log("country: ", country)
+
 
 	returnDate = (inboundDate == undefined) ?  new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000).toISOString().slice(0,10) : inboundDate.toString()
 	bodyCountry = (country == undefined) ? "US" : country.toString()
-	bodyCurrency = (currency == undefined) ? "USD" : currency.toString()
-	bodyLocale = (locale == undefined) ? "en-US" : locale.toString()
 	bodyOriginPlace = (originPlace == undefined) ? "DEN-sky" : originPlace.toString()
 	bodyDestinationPlace = (destinationPlace == undefined) ? "SFO-sky" : destinationPlace.toString()
 	departureDate = (outboundDate == undefined) ? new Date().toISOString().slice(0,10) : outboundDate.toString()
 
-	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/"+bodyCountry+"/"+bodyCurrency+
-		"/"+bodyLocale+"/"+bodyOriginPlace+"/"+bodyDestinationPlace+"/"+departureDate+"?inboundpartialdate="+returnDate, {
+	fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0/"+bodyCountry+"/"+currency+
+		"/"+locale+"/"+bodyOriginPlace+"/"+bodyDestinationPlace+"/"+departureDate+"?inboundpartialdate="+returnDate, {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
@@ -25,7 +32,10 @@ app.post('/findFlight', function(req, res) {
 	}
 	})
 	.then((response) => { 
-	    response.json().then((data) => {res.send(data)})
+	    response.json().then((data) => {
+	    	console.log(data)
+	    	res.send(data)
+	    })
     })
 
 });
